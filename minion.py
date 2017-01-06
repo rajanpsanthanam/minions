dd1d9c9ae079194d3988a875dad12a7432041637
rajan <rajanpsanthanam@gmail.com>
2017-01-07 01:13:33 +0530

    adding buckets

--- minion.py
+++ minion.py
@@ -8,94 +8,144 @@ from datetime import datetime
 
 DATE_FORMAT = '%l:%M%p %Z on %b %d, %Y'
 HOME = os.path.expanduser("~")
-PUSHIT_HOME = os.path.join(HOME, '.minion')
+MINION_HOME = os.path.join(HOME, '.minion')
 
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers(help='commands')
 
-# init
-init_parser = subparsers.add_parser('init', help='initialize empty bucket')
-init_parser.add_argument(
-    '--force', '-f', action='store_true', help='forcefully reinitialize'
-)
 
-# add
+# init
+init_parser = subparsers.add_parser('init', help='initialize minions')
+
+
+# create bucket
+create_parser = subparsers.add_parser('create', help='create empty bucket')
+create_parser.add_argument('name', help='name of the bucket')
+
+
+# delete bucket
+delete_parser = subparsers.add_parser('delete', help='delete a bucket')
+delete_parser.add_argument('name', help='name of the bucket')
+
+
+# add task
 add_parser = subparsers.add_parser('add', help='add a task to bucket')
+add_parser.add_argument('bucket', help='name of the bucket')
 add_parser.add_argument('name', help='name of the task')
 add_parser.add_argument('-d', '--desc', help='description of the task')
 
 
-# list
-list_parser = subparsers.add_parser('list', help='add a task to bucket')
-list_parser.add_argument('status', choices=['open', 'closed'])
+# list tasks
+list_parser = subparsers.add_parser('list', help='list the tasks from bucket')
+list_parser.add_argument('bucket', help='name of the bucket')
+list_parser.add_argument('-s', '--status', choices=['open', 'closed'])
 
 
-# close
+# close task
 close_parser = subparsers.add_parser('close', help='close a task in bucket')
+close_parser.add_argument('bucket', help='name of the bucket')
 close_parser.add_argument('name', help='name of the task')
 
 
-def _load_data():
-    location = os.path.join(PUSHIT_HOME, 'db.json')
-    data = json.load(open(location, 'r'))
-    return data
+def _load_data(bucket):
+    location = os.path.join(MINION_HOME, '{0}.json'.format(bucket))
+    if os.path.isfile(location):
+        data = json.load(open(location, 'r'))
+        return data
+    return None
 
 
-def _dump_data(data):
-    location = os.path.join(PUSHIT_HOME, 'db.json')
+def _dump_data(data, bucket):
+    location = os.path.join(MINION_HOME, '{0}.json'.format(bucket))
     json.dump(data, open(location, 'w'))
 
 
-def init(args):
-    if not os.path.isdir(PUSHIT_HOME):
-        os.mkdir(PUSHIT_HOME)
-        _dump_data([])
-    elif args.force:
-        _dump_data([])
+def _pretty_print(data):
+    print('status | task | description | start date | end date\n')
+    for item in data:
+        mark = 'X' if item.get('closed_date') else ''
+        print('[ {0} ] | {1} | {2} | {3} | {4}\n'.format(
+            mark, item.get('name'), item.get('desc'),
+            item.get('open_date'), item.get('closed_date', 'NA')
+        ))
+
+
+def init():
+    if not os.path.isdir(MINION_HOME):
+        os.mkdir(MINION_HOME)
     else:
         print('minions already loaded')
     return
 
 
+def create_bucket(args):
+    if not os.path.isdir(MINION_HOME):
+        print('run init first')
+    else:
+        _dump_data([], args.name)
+    return
+
+
+def delete_bucket(args):
+    if not os.path.isdir(MINION_HOME):
+        print('run init first')
+    else:
+        location = os.path.join(MINION_HOME, '{0}.json'.format(args.name))
+        os.remove(location)
+    return
+
+
 def add_task(args):
-    data = _load_data()
-    task_data = {
-        'name': args.name,
-        'desc': args.desc,
-        'status': 'open',
-        'open_date': datetime.now().strftime(DATE_FORMAT),
-        'closed_date': None
-    }
-    data.append(task_data)
-    _dump_data(data)
+    data = _load_data(args.bucket)
+    if data is None:
+        print('create bucket first')
+    else:
+        task_data = {
+            'name': args.name,
+            'desc': args.desc,
+            'status': 'open',
+            'open_date': datetime.now().strftime(DATE_FORMAT)
+        }
+        data.append(task_data)
+        _dump_data(data, args.bucket)
     return
 
 
 def close_task(args):
-    data = _load_data()
-    for item in data:
-        if item['name'] == args.name:
-            item['status'] = 'closed'
-            item['closed_date'] = datetime.now().strftime(DATE_FORMAT)
-    _dump_data(data)
+    data = _load_data(args.bucket)
+    if data is None:
+        print('create bucket first')
+    else:
+        for item in data:
+            if item['name'] == args.name:
+                item['status'] = 'closed'
+                item['closed_date'] = datetime.now().strftime(DATE_FORMAT)
+        _dump_data(data, args.bucket)
     return
 
 
 def list_tasks(args):
-    data = _load_data()
-    tasks = []
-    for item in data:
-        if item['status'] == args.status:
-            tasks.append(item)
-    for item in tasks:
-        print('{0} - {1} - {2} - {3}\n'.format(
-            item['name'], item['desc'], item['open_date'], item['closed_date']
-        ))
+    data = _load_data(args.bucket)
+    if data is None:
+        print('create bucket first')
+    else:
+        if args.status:
+            tasks = []
+            for item in data:
+                if item['status'] == args.status:
+                    tasks.append(item)
+            _pretty_print(tasks)
+        else:
+            _pretty_print(data)
 
 
 def handler(action, args):
     if action == 'init':
-        init(args)
+        init()
+    if action == 'create':
+        create_bucket(args)
+    if action == 'delete':
+        delete_bucket(args)
     if action == 'add':
         add_task(args)
     if action == 'list':
@@ -105,6 +155,9 @@ def handler(action, args):
 
 
 if __name__ == "__main__":
-    action = sys.argv[1]
-    args = parser.parse_args()
-    handler(action, args)
+    if len(sys.argv) == 1:
+        parser.print_help()
+    else:
+        action = sys.argv[1]
+        args = parser.parse_args()
+        handler(action, args)
